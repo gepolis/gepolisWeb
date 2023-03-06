bf67f2138dedc828f300889b1459e3bfcc17cb5f
gepolis <ivanaksenov2010@mail.ru>
2023-03-06 20:42:12 +0300

    Inited commit

---
+++ gepolisWeb/MainApp/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+
+def home(request):
+    return render(request,"pages/home.html")
+
+def order(request):
+    return render(request,"pages/order.html")
+
+def info(request):
+    return render(request,"pages/info.html")
+
+def about(request):
+    return render(request,"pages/about.html")
+
+def dashboard(request):
+    return render(request,"pages/dashboard/index.html")
+def dashboard_messages(request):
+    return render(request,"pages/dashboard/messages.html")
+def dashboard_chat(request,id):
+    return render(request,"pages/dashboard/chat.html")
