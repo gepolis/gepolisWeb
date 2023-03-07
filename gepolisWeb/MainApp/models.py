e7febd92948b7e10fbbde8f1711c02a8b0cb8d26
gepolis <ivanaksenov2010@mail.ru>
2023-03-07 12:55:19 +0300

    Add chat model

--- gepolisWeb/MainApp/models.py
+++ gepolisWeb/MainApp/models.py
@@ -1,2 +1,7 @@
 from django.db import models
 
+class Chats(models.Model):
+    fron = models.IntegerField()
+    to = models.IntegerField()
+    message = models.TextField(max_length=5000)
+    creation_date = models.DateTimeField(auto_now=True)
