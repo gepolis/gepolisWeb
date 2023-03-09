bbc12788a0aeb18decb4c0a98366af6db37272b9
gepolis <ivanaksenov2010@mail.ru>
2023-03-09 21:53:57 +0300

    add tages and project tages models

--- gepolisWeb/MainApp/models.py
+++ gepolisWeb/MainApp/models.py
@@ -7,3 +7,11 @@ class Projects(models.Model):
     use = models.TextField(max_length=1000)
     days = models.IntegerField(max_length=3)
     resault = models.TextField(max_length=100)
+
+class Tages(models.Model):
+    title = models.TextField(max_length=25)
+    color = models.TextField(max_length=7)
+
+class ProjectTages(models.Model):
+    projectId = models.IntegerField(max_length=11)
+    tageId = models.IntegerField(max_length=11)
