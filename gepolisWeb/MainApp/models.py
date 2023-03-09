b01a2f1f20831523300844676c3c03ab520a3b8b
gepolis <ivanaksenov2010@mail.ru>
2023-03-09 21:48:52 +0300

    Delete chat model, add projects model

--- gepolisWeb/MainApp/models.py
+++ gepolisWeb/MainApp/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 
-class Chats(models.Model):
-    fron = models.IntegerField()
-    to = models.IntegerField()
-    message = models.TextField(max_length=5000)
-    creation_date = models.DateTimeField(auto_now=True)
-    creation_time = models.TimeField(auto_now=True)
+class Projects(models.Model):
+    name = models.TextField(max_length=100)
+    desc = models.TextField(max_length=5000)
+    image = models.TextField(max_length=100)
+    use = models.TextField(max_length=1000)
+    days = models.IntegerField(max_length=3)
+    resault = models.TextField(max_length=100)
